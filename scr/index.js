93c465675a88203ce3ad63354931e3c8709e6432
emyrax <ekwueme416@gmail.com>
2026-01-05 10:21:56 +0100

    added cron

--- scr/index.js
+++ scr/index.js
@@ -6,14 +6,15 @@ import bookRoutes from "./routers/booksRoute.js"
 import mongoose from 'mongoose';
 import connectDB from './config/db.js'
 import cors from "cors"
-
+import job from "./config/cron.js"
 const app = express()
 const PORT = process.env.PORT ||3000
+job.start()
 connectDB()
 app.use(cors())
 app.use("/api/auth/",authRoutes)
 app.use("/api/books/",bookRoutes)
 
 app.listen(PORT , ()=>{
-  console.log(`listening... to ${PORT}`)
+  console.log(`listening... to updated ${PORT}`)
 })
